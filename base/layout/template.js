0b5e7d19519b622a03213cb05ddada0e39a7d0d2
gz_kazuma_seki <kazuma.seki@gzeal.co.jp>
2020-07-29 19:23:15 +0900

    全体テンプレート作成

--- base/layout/template.js
+++ base/layout/template.js
@@ -26,10 +26,47 @@ function header(){
   document.write(html);
 }
 
+function account(){
+  var html = "";
+  html += '<div class="hero is-info is-bold">';
+  html += '<div class="hero-body">';
+  html += '<div class="container">';
+  html += '<h1 class="title">CASAREAL</h1>';
+  html += '<h2 class="subtitle">@casareal-ls</h2>';
+  html += '</div>';
+  html += '</div>';
+  html += '</div>';
+  document.write(html);
+}
+
+function link(){
+  var html = "";
+  html += '<section class="level">';
+  html += '<div class="level-left"></div>';
+  html += '<div class="level-right">';
+  html += '<div class="level-item tabs">';
+  html += '<ul>';
+  html += '<li class="is-active"><a>最近の一言</a></li>';
+  html += '<li><a>古い一言</a></li>';
+  html += '<li><a>コメント付き</a></li>';
+  html += '</ul>';
+  html += '</div>';
+  html += '</div>';
+  html += '<div class="level-right">';
+  html += '<div class="level-item">';
+  html += '<a class="button is-greylight">フォロー</a>';
+  html += '</div>';
+  html += '</div>';
+  html += '</section>';
+  document.write(html);
+}
+
 function footer(){
   var html = "";
-  html += '<footer>';
-  html += '<p><small>copyright ﾂｩ All Rights Reserved.</small></p>';
+  html += '<footer class="footer">';
+  html += '<div class="container">';
+  html += '<div class="content has-text-centered">2020 ©CASAREAL</div>';
+  html += '</div>';
   html += '</footer>';
   document.write(html);
 }
